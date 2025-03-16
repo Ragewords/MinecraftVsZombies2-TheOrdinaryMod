3675144a51cd74d4da927be12acf8428625f3edd
生气字 <1091533403@qq.com>
2025-03-16 12:03:13 +0800

    完善月狂难度属性。

---
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Contraption/LunaticContraptionBuff.cs
@@ -0,0 +1,16 @@
+using MVZ2.Vanilla.Entities;
+using PVZEngine.Buffs;
+using PVZEngine.Level;
+using PVZEngine.Modifiers;
+
+namespace MVZ2.GameContent.Buffs.Contraptions
+{
+    [BuffDefinition(VanillaBuffNames.lunaticContraption)]
+    public class LunaticContraptionBuff : BuffDefinition
+    {
+        public LunaticContraptionBuff(string nsp, string name) : base(nsp, name)
+        {
+            AddModifier(new FloatModifier(VanillaEntityProps.DAMAGE, NumberOperator.AddMultiplie, -0.25f));
+        }
+    }
+}
