6460edb100fc20f8dcf8281632ce6c6772e7a04e
生气字 <1091533403@qq.com>
2025-06-28 14:14:25 +0800

    将地狱战车的飞行高度从0变成1，修正地狱战车的模型。

--- Assets/Scripts/Vanilla/GameContent/Enemies/Chapter3/HellChariot.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/Chapter3/HellChariot.cs
@@ -33,7 +33,7 @@ namespace MVZ2.GameContent.Enemies
                 entity.Level.AddLoopSoundEntity(VanillaSoundID.trainTravel, entity.ID);
             }
             var buff = entity.AddBuff<FlyBuff>();
-            buff.SetProperty(FlyBuff.PROP_TARGET_HEIGHT, 0f);
+            buff.SetProperty(FlyBuff.PROP_TARGET_HEIGHT, 1f);
         }
         protected override void UpdateLogic(Entity entity)
         {
