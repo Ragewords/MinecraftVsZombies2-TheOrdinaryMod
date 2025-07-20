c6c40380e9501b3d322514b794c3fc7bf138df6f
生气字 <1091533403@qq.com>
2025-07-20 22:27:32 +0800

    整理代码

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/YoukaiLeaf.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/YoukaiLeaf.cs
@@ -1,14 +1,10 @@
-using System.Collections.Generic;
 using MVZ2.GameContent.Buffs.Enemies;
-using MVZ2.GameContent.Detections;
 using MVZ2.GameContent.Effects;
 using MVZ2.Vanilla.Audios;
 using MVZ2.Vanilla.Callbacks;
 using MVZ2.Vanilla.Contraptions;
-using MVZ2.Vanilla.Detections;
 using MVZ2.Vanilla.Entities;
 using PVZEngine.Callbacks;
-using PVZEngine.Damages;
 using PVZEngine.Entities;
 using PVZEngine.Level;
 using UnityEngine;
