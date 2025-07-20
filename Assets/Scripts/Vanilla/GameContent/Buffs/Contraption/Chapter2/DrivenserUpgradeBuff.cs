1ec6b19ce6c95575151a03fea1e88f570f66d8ee
生气字 <1091533403@qq.com>
2025-07-20 13:24:09 +0800

    驱动发射器现在在升级时只增加射速和子弹飞行速度。

--- Assets/Scripts/Vanilla/GameContent/Buffs/Contraption/Chapter2/DrivenserUpgradeBuff.cs
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Contraption/Chapter2/DrivenserUpgradeBuff.cs
@@ -11,9 +11,8 @@ namespace MVZ2.GameContent.Buffs.Contraptions
     {
         public DrivenserUpgradeBuff(string nsp, string name) : base(nsp, name)
         {
-            AddModifier(new FloatModifier(VanillaEntityProps.DAMAGE, NumberOperator.Add, 2));
-            AddModifier(new Vector3Modifier(VanillaEntityProps.SHOT_VELOCITY, NumberOperator.Multiply, Vector3.one * 1.1f));
-            AddModifier(new FloatModifier(VanillaEntityProps.ATTACK_SPEED, NumberOperator.Add, 1));
+            AddModifier(new Vector3Modifier(VanillaEntityProps.SHOT_VELOCITY, NumberOperator.Multiply, Vector3.one * 1.25f));
+            AddModifier(new FloatModifier(VanillaEntityProps.ATTACK_SPEED, NumberOperator.Add, 1.25f));
         }
     }
 }
