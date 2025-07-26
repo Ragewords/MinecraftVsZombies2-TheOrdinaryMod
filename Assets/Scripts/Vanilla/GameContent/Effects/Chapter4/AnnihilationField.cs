51de8b164449802c519bca2950fd475e9c5de4c6
生气字 <1091533403@qq.com>
2025-07-26 21:02:50 +0800

    修复湮灭领域不会消除敌弹的问题。

--- Assets/Scripts/Vanilla/GameContent/Effects/Chapter4/AnnihilationField.cs
+++ Assets/Scripts/Vanilla/GameContent/Effects/Chapter4/AnnihilationField.cs
@@ -25,6 +25,7 @@ namespace MVZ2.GameContent.Effects
         {
             absorbDetector = new BlackholeDetector()
             {
+                mask = EntityCollisionHelper.MASK_VULNERABLE | EntityCollisionHelper.MASK_PROJECTILE,
                 canDetectInvisible = true,
                 factionTarget = FactionTarget.Hostile
             };
