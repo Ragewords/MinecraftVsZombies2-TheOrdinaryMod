1474a4657f4fe958ff30e12f2902f55fe00f4fb1
生气字 <1091533403@qq.com>
2025-07-28 09:28:09 +0800

    删除了萤火枯骨。

--- Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/VanillaEnemyID.cs
@@ -50,7 +50,6 @@ namespace MVZ2.GameContent.Enemies
         public const string boneWall = "bone_wall";
         public const string bomb = "bomb";
         public const string napstablook = "napstablook";
-        public const string willoSkull = "willo_skull";
         public const string reverseSatellite = "reverse_satellite";
         public const string skeletonHorse = "skeleton_horse";
         public const string dullahanHead = "dullahan_head";
@@ -116,7 +115,6 @@ namespace MVZ2.GameContent.Enemies
         public static readonly NamespaceID boneWall = Get(VanillaEnemyNames.boneWall);
         public static readonly NamespaceID bomb = Get(VanillaEnemyNames.bomb);
         public static readonly NamespaceID napstablook = Get(VanillaEnemyNames.napstablook);
-        public static readonly NamespaceID willoSkull = Get(VanillaEnemyNames.willoSkull);
         public static readonly NamespaceID reverseSatellite = Get(VanillaEnemyNames.reverseSatellite);
         public static readonly NamespaceID skeletonHorse = Get(VanillaEnemyNames.skeletonHorse);
         public static readonly NamespaceID dullahanHead = Get(VanillaEnemyNames.dullahanHead);
