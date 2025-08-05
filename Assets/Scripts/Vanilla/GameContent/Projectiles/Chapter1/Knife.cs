874e841655faa9500006f99fb9d40862096ab96c
生气字 <1091533403@qq.com>
2025-08-05 13:31:11 +0800

    修改了金属反弹尖锐物体的方向计算方式。

--- Assets/Scripts/Vanilla/GameContent/Projectiles/Chapter1/Knife.cs
+++ Assets/Scripts/Vanilla/GameContent/Projectiles/Chapter1/Knife.cs
@@ -98,58 +98,12 @@ namespace MVZ2.GameContent.Projectiles
         public static readonly VanillaEntityPropertyMeta<FrameTimer> PROP_WAIT_TIMER = new VanillaEntityPropertyMeta<FrameTimer>("WaitTimer");
         public static void Deflect(Entity self, Entity knife)
         {
-            var size = self.GetScaledSize();
             float thisX = self.Position.x;
             float thisZ = self.Position.z;
-            float xExtent = size.x / 2;
-            float zExtent = size.z / 2;
             float knifeX = knife.Position.x;
             float knifeZ = knife.Position.z;
-            float knifeXSpeed = knife.Velocity.x;
-            float knifeZSpeed = knife.Velocity.z;
 
-            Vector2 leftUp = new Vector2(thisX - xExtent, thisZ + zExtent);
-            Vector2 leftDown = new Vector2(thisX - xExtent, thisZ - zExtent);
-            Vector2 rightUp = new Vector2(thisX + xExtent, thisZ + zExtent);
-            Vector2 rightDown = new Vector2(thisX + xExtent, thisZ - zExtent);
-
-            Vector2 otherDirection2D = new Vector2(knifeXSpeed, knifeZSpeed).normalized;
-            Vector2 longRange = otherDirection2D * 100;
-            Vector2 otherLastPos2D = new Vector2(knifeX, knifeZ) - longRange;
-            Vector2 otherPos2D = new Vector2(knifeX, knifeZ) + longRange;
-
-            Vector3 normal = Vector3.zero;
-
-            if (knifeZSpeed > 0 && knifeZ <= self.Position.z)
-            {
-                if (MathTool.DoLinesIntersect(otherLastPos2D, otherPos2D, leftDown, rightDown))
-                {
-                    normal = Vector3.back;
-                }
-            }
-            else if (knifeZSpeed < 0 && knifeZ >= self.Position.z)
-            {
-                if (MathTool.DoLinesIntersect(otherLastPos2D, otherPos2D, leftUp, rightUp))
-                {
-                    normal = Vector3.forward;
-                }
-            }
-
-            if (knifeXSpeed > 0 && knifeX <= self.Position.x)
-            {
-                if (MathTool.DoLinesIntersect(otherLastPos2D, otherPos2D, leftUp, leftDown))
-                {
-                    normal = Vector3.left;
-                }
-            }
-            else if (knifeXSpeed < 0 && knifeX >= self.Position.x)
-            {
-                if (MathTool.DoLinesIntersect(otherLastPos2D, otherPos2D, rightUp, rightDown))
-                {
-                    normal = Vector3.right;
-                }
-            }
-            knife.Velocity = Vector3.Reflect(knife.Velocity, normal.normalized);
+            knife.Velocity = Vector3.Reflect(knife.Velocity, new Vector3(thisX - knifeX, 0, thisZ - knifeZ).normalized);
         }
     }
 }
