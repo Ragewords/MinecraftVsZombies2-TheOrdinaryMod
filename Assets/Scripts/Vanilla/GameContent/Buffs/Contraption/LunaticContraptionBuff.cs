9557bb795f3a742988304a4b5afea1f6c9992159
生气字 <1091533403@qq.com>
2025-08-12 08:14:18 +0800

    修改月狂难度下的一些器械削弱和敌人增强。

--- Assets/Scripts/Vanilla/GameContent/Buffs/Contraption/LunaticContraptionBuff.cs
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Contraption/LunaticContraptionBuff.cs
@@ -2,6 +2,7 @@ using MVZ2.Vanilla.Entities;
 using PVZEngine.Buffs;
 using PVZEngine.Level;
 using PVZEngine.Modifiers;
+using UnityEngine;
 
 namespace MVZ2.GameContent.Buffs.Contraptions
 {
@@ -11,7 +12,7 @@ namespace MVZ2.GameContent.Buffs.Contraptions
         public LunaticContraptionBuff(string nsp, string name) : base(nsp, name)
         {
             AddModifier(new FloatModifier(VanillaEntityProps.PRODUCE_SPEED, NumberOperator.Multiply, 0.75f));
-            AddModifier(new FloatModifier(VanillaEntityProps.ATTACK_SPEED, NumberOperator.Multiply, 0.75f));
+            AddModifier(new Vector3Modifier(VanillaEntityProps.LIGHT_RANGE, NumberOperator.Multiply, Vector3.one * 0.75f));
         }
     }
 }
