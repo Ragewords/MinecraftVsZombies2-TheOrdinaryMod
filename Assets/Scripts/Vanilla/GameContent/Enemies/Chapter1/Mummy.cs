6021205ec6b31904f8e27f09f01569762e17d732
生气字 <1091533403@qq.com>
2025-08-13 00:26:32 +0800

    清理代码

--- Assets/Scripts/Vanilla/GameContent/Enemies/Chapter1/Mummy.cs
+++ Assets/Scripts/Vanilla/GameContent/Enemies/Chapter1/Mummy.cs
@@ -1,11 +1,8 @@
-﻿using MVZ2.GameContent.Armors;
-using MVZ2.GameContent.Buffs.Enemies;
-using MVZ2.GameContent.Damages;
+﻿using MVZ2.GameContent.Damages;
 using MVZ2.GameContent.Effects;
 using MVZ2.Vanilla.Audios;
 using MVZ2.Vanilla.Enemies;
 using MVZ2.Vanilla.Entities;
-using MVZ2.Vanilla.Level;
 using PVZEngine.Damages;
 using PVZEngine.Entities;
 using PVZEngine.Level;
@@ -17,16 +14,6 @@ namespace MVZ2.GameContent.Enemies
     {
         public Mummy(string nsp, string name) : base(nsp, name)
         {
-        }
-        public override void Init(Entity entity)
-        {
-            base.Init(entity);
-            var level = entity.Level;
-            if (level.IsWaterLane(entity.GetLane()))
-            {
-                entity.AddBuff<BoatBuff>();
-                entity.SetAnimationBool("HasBoat", true);
-            }
         }
         protected override void UpdateLogic(Entity entity)
         {
@@ -41,12 +28,9 @@ namespace MVZ2.GameContent.Enemies
             var param = entity.GetSpawnParams();
             param.SetProperty(EngineEntityProps.SCALE, entity.GetScale());
             entity.Spawn(VanillaEffectID.mummyGas, entity.Position, param);
-            entity.SpawnWithParams(VanillaEnemyID.zombie, entity.Position);
             entity.PlaySound(VanillaSoundID.poisonCast);
+            entity.SpawnWithParams(VanillaEnemyID.zombie, entity.Position);
             entity.Remove();
-
-            var fragment = entity.CreateFragment();
-            Fragment.AddEmitSpeed(fragment, 500);
         }
     }
 }
