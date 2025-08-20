fe9fe9ec4eb116fea5c61c225c3a2268b084e130
生气字 <1091533403@qq.com>
2025-08-20 15:11:40 +0800

    修改音浪的体积。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/NoteBlock.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/NoteBlock.cs
@@ -122,7 +122,7 @@ namespace MVZ2.GameContent.Contraptions
                 var rangeMultiplier = entity.HasBuff<NoteBlockLoudBuff>() ? 2 : 1;
                 var param = entity.GetSpawnParams();
                 param.SetProperty(VanillaEntityProps.DAMAGE, entity.GetDamage() * 0.4f);
-                param.SetProperty(EngineEntityProps.SIZE, new Vector3(240, 80, 240) * rangeMultiplier);
+                param.SetProperty(EngineEntityProps.SCALE, Vector3.one * rangeMultiplier);
                 var wave = entity.Spawn(VanillaEffectID.soundwave, entity.GetCenter(), param);
                 Soundwave.SetLoud(wave, entity.HasBuff<NoteBlockLoudBuff>());
                 waveTimer.Reset();
