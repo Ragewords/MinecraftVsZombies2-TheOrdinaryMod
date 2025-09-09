9e92721a630695f6eb0f6ef5c82f711df1293c54
生气字 <1091533403@qq.com>
2025-09-09 16:50:55 +0800

    调整重低音鼓的伤害加成。

--- Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter3/HeavyBassDrum.cs
+++ Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter3/HeavyBassDrum.cs
@@ -36,7 +36,7 @@ namespace MVZ2.GameContent.Artifacts
             }
             if (valid)
             {
-                projectile.SplashDamage(excludeCollider, projectile.GetCenter(), 20, projectile.GetFaction(), projectile.GetDamage() * 0.2f, new DamageEffectList(VanillaDamageEffects.MUTE));
+                projectile.SplashDamage(excludeCollider, projectile.GetCenter(), 20, projectile.GetFaction(), projectile.GetDamage() * 0.05f, new DamageEffectList(VanillaDamageEffects.MUTE));
             }
         }
     }
