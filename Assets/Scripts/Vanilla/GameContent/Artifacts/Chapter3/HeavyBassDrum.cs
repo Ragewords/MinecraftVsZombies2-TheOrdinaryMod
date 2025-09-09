5f7c6c33cf6a4cfb285a63c7fa82e3633033aebe
生气字 <1091533403@qq.com>
2025-09-09 16:47:45 +0800

    修改了重低音鼓的效果。

--- Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter3/HeavyBassDrum.cs
+++ Assets/Scripts/Vanilla/GameContent/Artifacts/Chapter3/HeavyBassDrum.cs
@@ -1,10 +1,11 @@
-using MVZ2.Vanilla.Audios;
+using MVZ2.GameContent.Damages;
 using MVZ2.Vanilla.Callbacks;
 using MVZ2.Vanilla.Entities;
 using MVZ2Logic;
 using MVZ2Logic.Artifacts;
 using MVZ2Logic.Level;
 using PVZEngine.Callbacks;
+using PVZEngine.Damages;
 
 namespace MVZ2.GameContent.Artifacts
 {
@@ -13,77 +14,30 @@ namespace MVZ2.GameContent.Artifacts
     {
         public HeavyBassDrum(string nsp, string name) : base(nsp, name)
         {
-            AddTrigger(VanillaLevelCallbacks.POST_BODY_TAKE_DAMAGE, PostBodyTakeDamageCallback);
-            AddTrigger(VanillaLevelCallbacks.POST_ARMOR_TAKE_DAMAGE, PostArmorTakeDamageCallback);
+            AddTrigger(VanillaLevelCallbacks.POST_PROJECTILE_HIT, PostProjectileHitCallback);
         }
-        public override void PostUpdate(Artifact artifact)
+        private void PostProjectileHitCallback(VanillaLevelCallbacks.PostProjectileHitParams hitParams, CallbackResult result)
         {
-            base.PostUpdate(artifact);
-            artifact.SetGlowing(true);
-            AddArtifactTime();
-        }
-        private void PostBodyTakeDamageCallback(VanillaLevelCallbacks.PostBodyTakeDamageParams param, CallbackResult result)
-        {
-            var entity = param.output?.Entity;
-            if (entity == null)
+            var projectile = hitParams.hit.Projectile;
+            if (projectile.IsHostileEntity())
                 return;
-            if (!entity.IsHostileEntity())
-                return;
-            var level = entity.Level;
-            var artifacts = level.GetArtifacts();
+            var excludeCollider = hitParams.hit.Collider;
+            var level = projectile.Level;
             bool valid = false;
+            var artifacts = level.GetArtifacts();
             foreach (var artifact in artifacts)
             {
                 if (artifact == null)
                     continue;
                 if (artifact.Definition != this)
                     continue;
-                if (!IsTimeInterval(60))
-                    continue;
                 artifact.Highlight();
                 valid = true;
             }
-            if (valid && entity.CanDeactive())
+            if (valid)
             {
-                entity.Stun(30);
-                entity.PlaySound(VanillaSoundID.punch);
+                projectile.SplashDamage(excludeCollider, projectile.GetCenter(), 20, projectile.GetFaction(), projectile.GetDamage() * 0.2f, new DamageEffectList(VanillaDamageEffects.MUTE));
             }
         }
-        private void PostArmorTakeDamageCallback(VanillaLevelCallbacks.PostArmorTakeDamageParams param, CallbackResult result)
-        {
-            var entity = param.result?.Entity;
-            if (entity == null)
-                return;
-            if (!entity.IsHostileEntity())
-                return;
-            var level = entity.Level;
-            var artifacts = level.GetArtifacts();
-            bool valid = false;
-            foreach (var artifact in artifacts)
-            {
-                if (artifact == null)
-                    continue;
-                if (artifact.Definition != this)
-                    continue;
-                if (!IsTimeInterval(60))
-                    continue;
-                artifact.Highlight();
-                valid = true;
-            }
-            if (valid && entity.CanDeactive())
-            {
-                entity.Stun(30);
-                entity.PlaySound(VanillaSoundID.punch);
-            }
-        }
-        private void AddArtifactTime()
-        {
-            artifactTime++;
-        }
-        private bool IsTimeInterval(long interval, long offset = 0)
-        {
-            return artifactTime % interval == offset;
-        }
-        private long artifactTime = 0;
     }
 }
