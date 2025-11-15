72ef33798119435e0129eda416b4ccaefaad9c8b
生气字 <1091533403@qq.com>
2025-11-15 12:44:33 +0800

    adjust：火药桶的最低生产速度变为30秒，每多一个火药桶减少1.5秒。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter5/GunpowderBarrel.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter5/GunpowderBarrel.cs
@@ -147,8 +147,8 @@ namespace MVZ2.GameContent.Contraptions
         public const int PRODUCTION_TIME_START_MIN = 90;
         public const int PRODUCTION_TIME_START_MAX = 360;
         public const int PRODUCTION_TIME = 1080;
-        public const int PRODUCTION_TIME_REDUCE_MIN = 720;
-        public const int PRODUCTION_TIME_REDUCTION = 30;
+        public const int PRODUCTION_TIME_REDUCE_MIN = 900;
+        public const int PRODUCTION_TIME_REDUCTION = 45;
         public const int MAX_COUNT = (PRODUCTION_TIME - PRODUCTION_TIME_REDUCE_MIN) / PRODUCTION_TIME_REDUCTION;
         private static readonly VanillaEntityPropertyMeta<FrameTimer> PROP_PRODUCTION_TIMER = new VanillaEntityPropertyMeta<FrameTimer>("ProductionTimer");
         private static readonly VanillaEntityPropertyMeta<bool> PROP_FURIOUS = new VanillaEntityPropertyMeta<bool>("fury");
