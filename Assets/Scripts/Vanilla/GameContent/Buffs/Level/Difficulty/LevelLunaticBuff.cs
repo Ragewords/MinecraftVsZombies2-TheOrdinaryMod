4a77097f72cead42b8fa165291716afbda785366
生气字 <1091533403@qq.com>
2025-12-14 16:59:01 +0800

    adjust： - 地雷TNT的伤害降低至900。 - 降低月狂难度的出怪。

--- Assets/Scripts/Vanilla/GameContent/Buffs/Level/Difficulty/LevelLunaticBuff.cs
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Level/Difficulty/LevelLunaticBuff.cs
@@ -21,7 +21,6 @@ namespace MVZ2.GameContent.Buffs.Level
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.GUNPOWDER_DAMAGE_MULTIPLIER, NumberOperator.Multiply, 4));
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.ELASTIC_CLOUD_BOUNCE_DAMAGE_MULTIPLIER, NumberOperator.Multiply, 1.5f));
 
-            AddModifier(new FloatModifier(VanillaLevelProps.SPAWN_POINTS_POWER, NumberOperator.AddMultiple, 0.2f));
             AddModifier(new FloatModifier(VanillaLevelProps.SPAWN_POINTS_ADDITION, NumberOperator.Add, 1f));
             AddModifier(new IntModifier(VanillaDifficultyLevelProps.NAPSTABLOOK_PARALYSIS_TIME, IntegerOperator.Multiply, 2));
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.GHAST_DAMAGE_MULTIPLIER, NumberOperator.Add, 1f));
