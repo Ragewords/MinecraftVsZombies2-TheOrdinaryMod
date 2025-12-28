f1df1c8cb4ee36c856f94a0711ac4fbccff564df
生气字 <1091533403@qq.com>
2025-12-29 01:26:25 +0800

    revert：回退对于连射发射器和高频脉冲发射器的调整。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter5/Repeatenser.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter5/Repeatenser.cs
@@ -4,7 +4,6 @@ using MVZ2.GameContent.Projectiles;
 using MVZ2.Vanilla.Audios;
 using MVZ2.Vanilla.Contraptions;
 using MVZ2.Vanilla.Entities;
-using MVZ2.Vanilla.Level;
 using MVZ2.Vanilla.Properties;
 using PVZEngine.Entities;
 using PVZEngine.Level;
@@ -69,14 +68,6 @@ namespace MVZ2.GameContent.Contraptions
                 repeatTimer.ResetTime(Mathf.FloorToInt(15f / count));
                 repeatTimer.Frame = 0;
             }
-        }
-        protected override int GetTimerTime(Entity entity)
-        {
-            if (entity.Level.IsIZombie())
-            {
-                return ATTACK_INTERVAL_MAX;
-            }
-            return entity.RNG.Next(ATTACK_INTERVAL_MIN, ATTACK_INTERVAL_MAX + 1);
         }
 
         protected override void OnEvoke(Entity entity)
@@ -170,8 +161,6 @@ namespace MVZ2.GameContent.Contraptions
         public static void SetDoubleShoot(Entity entity, bool value) => entity.SetBehaviourField(PROP_DOUBLE_SHOOT, value);
 
         public const int REPEAT_COUNT = 2;
-        private const int ATTACK_INTERVAL_MIN = 55;
-        private const int ATTACK_INTERVAL_MAX = 60;
         public static readonly VanillaEntityPropertyMeta<FrameTimer> PROP_EVOCATION_TIMER = new VanillaEntityPropertyMeta<FrameTimer>("EvocationTimer");
         public static readonly VanillaEntityPropertyMeta<FrameTimer> PROP_REPEAT_TIMER = new VanillaEntityPropertyMeta<FrameTimer>("RepeatTimer");
         public static readonly VanillaEntityPropertyMeta<bool> PROP_DOUBLE_SHOOT = new VanillaEntityPropertyMeta<bool>("DoubleShoot");
