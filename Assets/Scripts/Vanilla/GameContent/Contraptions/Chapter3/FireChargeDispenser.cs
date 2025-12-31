1e7a2b82e3b851774d430addac018114c9167782
生气字 <1091533403@qq.com>
2025-12-31 13:54:30 +0800

    balance：吞噬者费用0->25。 balance：增加火焰弹发射器大招的总子弹数。费用175->150。冷却时间长->中等。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter3/FireChargeDispenser.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter3/FireChargeDispenser.cs
@@ -142,7 +142,7 @@ namespace MVZ2.GameContent.Contraptions
         public static void SetEvocationTimer(Entity entity, FrameTimer timer) => entity.SetBehaviourField(ID, PROP_MISSLE_TIMEOUT, timer);
 
         public const int ATTACK_INTERVAL = 60;
-        public const int EVOKATION_TIMER = 30;
+        public const int EVOKATION_TIMER = 60;
         public const int GRAVITY = 1;
         public const float ATTACK_HEIGHT = 160;
         public static readonly VanillaEntityPropertyMeta<FrameTimer> PROP_ATTACK_TIMER = new VanillaEntityPropertyMeta<FrameTimer>("AttackTimer");
