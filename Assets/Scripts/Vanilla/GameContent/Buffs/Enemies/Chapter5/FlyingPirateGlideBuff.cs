4aa6816eac938cd0fe771924b91da64a1521955d
生气字 <1091533403@qq.com>
2026-01-14 12:48:04 +0800

    fix：修复诸如火焰弹一类的弹射物命中闪电之珠后不会记录伤害的问题。

--- Assets/Scripts/Vanilla/GameContent/Buffs/Enemies/Chapter5/FlyingPirateGlideBuff.cs
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Enemies/Chapter5/FlyingPirateGlideBuff.cs
@@ -3,6 +3,7 @@
 using MVZ2.GameContent.Enemies;
 using MVZ2.Vanilla.Enemies;
 using MVZ2.Vanilla.Entities;
+using MVZ2.Vanilla.Modifiers;
 using MVZ2.Vanilla.Properties;
 using PVZEngine.Buffs;
 using PVZEngine.Entities;
@@ -17,7 +18,7 @@ namespace MVZ2.GameContent.Buffs.Enemies
         public FlyingPirateGlideBuff(string nsp, string name) : base(nsp, name)
         {
             AddModifier(new FloatModifier(EngineEntityProps.GRAVITY, NumberOperator.Multiply, 0));
-            AddModifier(new FloatModifier(VanillaEnemyProps.SPEED, NumberOperator.Set, 10, 10000));
+            AddModifier(new FloatModifier(VanillaEnemyProps.SPEED, NumberOperator.Set, 10, VanillaModifierPriorities.FORCE));
             AddModifier(new IntModifier(VanillaEnemyProps.STATE_OVERRIDE, IntegerOperator.Set, VanillaEnemyStates.IDLE));
         }
         public override void PostAdd(Buff buff)
