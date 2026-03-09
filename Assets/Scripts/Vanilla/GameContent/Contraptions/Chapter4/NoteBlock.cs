56e4a2eb2950f7fdd7856fae2093d3a50f13f1a6
生气字 <1091533403@qq.com>
2026-03-09 15:59:16 +0800

    balance：唱片机的音浪伤害降低至1。

--- Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/NoteBlock.cs
+++ Assets/Scripts/Vanilla/GameContent/Contraptions/Chapter4/NoteBlock.cs
@@ -122,7 +122,7 @@ namespace MVZ2.GameContent.Contraptions
         {
             entity.TriggerAnimation("Sound");
             var rangeMultiplier = entity.HasBuff<NoteBlockLoudBuff>() ? 2 : 1;
-            entity.Explode(entity.GetCenter(), SOUND_RADIUS * rangeMultiplier, entity.GetFaction(), entity.GetDamage() * SOUND_DAMAGE_MULTIPLIER, new DamageEffectList(VanillaDamageEffects.MUTE));
+            entity.Explode(entity.GetCenter(), SOUND_RADIUS * rangeMultiplier, entity.GetFaction(), entity.GetDamage() / 3f, new DamageEffectList(VanillaDamageEffects.MUTE));
         }
         public static List<EntityID>? GetNoteChildren(Entity entity)
         {
@@ -146,7 +146,6 @@ namespace MVZ2.GameContent.Contraptions
         public const int FIRE_INTERVAL = 45;
         public const int WAVE_INTERVAL = 5;
         public const int SOUND_RADIUS = 120;
-        public const float SOUND_DAMAGE_MULTIPLIER = 0.66666666666f;
         public const int MAX_NOTE_COUNT = 10;
         public static FrameTimer? GetWaveTimer(Entity entity) => entity.GetBehaviourField<FrameTimer>(PROP_WAVE_TIMER);
         public static void SetWaveTimer(Entity entity, FrameTimer timer) => entity.SetBehaviourField(PROP_WAVE_TIMER, timer);
