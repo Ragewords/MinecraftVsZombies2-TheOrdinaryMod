cf5695d559fa725ea68539192cc368bbd9aacabe
生气字 <1091533403@qq.com>
2026-04-21 22:47:23 +0800

    再次移除月狂难度下的怪物调整。

--- Assets/Scripts/Vanilla/GameContent/Buffs/Level/Difficulty/LevelLunaticBuff.cs
+++ Assets/Scripts/Vanilla/GameContent/Buffs/Level/Difficulty/LevelLunaticBuff.cs
@@ -21,8 +21,9 @@ namespace MVZ2.GameContent.Buffs.Level
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.GUNPOWDER_DAMAGE_MULTIPLIER, NumberOperator.Multiply, 4));
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.ELASTIC_CLOUD_BOUNCE_DAMAGE_MULTIPLIER, NumberOperator.Multiply, 1.5f));
 
+            AddModifier(new BooleanModifier(VanillaDifficultyLevelProps.USE_LUNATIC_PROPERTY, true));
             AddModifier(new FloatModifier(VanillaLevelProps.SPAWN_POINTS_ADDITION, NumberOperator.Add, 1f));
-            AddModifier(new FloatModifier(VanillaLevelProps.SPAWN_POINTS_POWER, NumberOperator.AddMultiple, 0.2f));
+            AddModifier(new FloatModifier(VanillaLevelProps.SPAWN_POINTS_POWER, NumberOperator.AddMultiple, 0.1f));
             AddModifier(new IntModifier(VanillaDifficultyLevelProps.NAPSTABLOOK_PARALYSIS_TIME, IntegerOperator.Multiply, 2));
             AddModifier(new FloatModifier(VanillaDifficultyLevelProps.GHAST_DAMAGE_MULTIPLIER, NumberOperator.Add, 1f));
             AddModifier(new IntModifier(VanillaDifficultyLevelProps.MOTHER_TERROR_EGG_COUNT, IntegerOperator.Add, 1));
